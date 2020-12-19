bdf6d861ba19f543275ba512beb5dc63eec20ef1
shenghanqin <shenghanqin@163.com>
2020-12-19 09:39:48 +0800

    微调样式

--- components/footer/index.js
+++ components/footer/index.js
@@ -4,7 +4,7 @@ export default function Footer(props) {
   return (
     <footer className="footer">
       <div className="footer-content">
-        Copyright © 2020 华葳 All Rights Reserved. <a href="http://www.beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">沪ICP备20023198号</a>
+        Copyright © 2020 华葳 All Rights Reserved. <br /><a href="http://www.beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">沪ICP备20023198号</a>
       </div>
     </footer>
   )
